9af901efe01e941ca3a8af88cfdbe7dfc9f5b332
Felipe <34971689+AndrewIA961@users.noreply.github.com>
2023-01-18 01:05:06 -0500

    Update Leetcode_validAnagram.js

--- Leetcode_validAnagram.js
+++ Leetcode_validAnagram.js
@@ -17,18 +17,4 @@ var isAnagram = function (s, t) {
 
 
 
-/*var isAnagram = function(s, t) {
-    /*if(s.length !== t.length){
-        return false
-    }
-};
-    function sorter (str){
-        console.log(str.split('').sort())
-    }
 
-    //const sSorted = s.split("").sort("").join("")
-    //const tSorted = s.split("").sort("").join("")
-    //return sSorted === tSorted
-
-
-sorter('hello')*/
